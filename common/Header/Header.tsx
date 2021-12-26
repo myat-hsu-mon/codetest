8f0a6e90aef835d48fd9e86dde00903f65914d1e
MrPhyaeSoneThwim <mr.phyaesonethwim1998@gmail.com>
2021-12-26 15:36:10 +0630

    added responsive navbar ui

--- common/Header/Header.tsx
+++ common/Header/Header.tsx
@@ -1,57 +1,66 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { FiX } from "react-icons/fi";
 
 import Logo from "../../public/logo-codigo-red.svg";
 import headerStyles from "./Header.module.scss";
 
 const navs = [
-  {
-    name: "Work",
-    href: "/work",
-  },
-  {
-    name: "Solutions",
-    href: "/solutions",
-  },
-  {
-    name: "Services",
-    href: "/services",
-  },
-  {
-    name: "About us",
-    href: "/about-us",
-  },
-  {
-    name: "Blog",
-    href: "/blog",
-  },
+	{
+		name: "Work",
+		href: "/work",
+	},
+	{
+		name: "Solutions",
+		href: "/solutions",
+	},
+	{
+		name: "Services",
+		href: "/services",
+	},
+	{
+		name: "About us",
+		href: "/about-us",
+	},
+	{
+		name: "Blog",
+		href: "/blog",
+	},
 ];
 
 const Header: NextPage = () => (
-  <header className={headerStyles.header}>
-    <nav className={headerStyles.navbar}>
-      <Link href="#" passHref>
-        <a className={headerStyles.navLogo}>
-          <Image src={Logo} alt="Codico Logo" width={145} height={33} />
-        </a>
-      </Link>
-      <ul className={headerStyles.navMenu}>
-        {navs.map(({ name, href }) => (
-          <li className={headerStyles.navItem} key={name}>
-            <Link href={href} passHref>
-              <a className={headerStyles.navLink}>{name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className={headerStyles.hamburger}>
-        <span className={headerStyles.bar}></span>
-        <span className={headerStyles.bar}></span>
-        <span className={headerStyles.bar}></span>
-      </div>
-    </nav>
-  </header>
+	<header className={headerStyles.header}>
+		<nav className={headerStyles.navbar}>
+			<Link href="#" passHref>
+				<a className={headerStyles.navLogo}>
+					<Image src={Logo} alt="Codico Logo" width={145} height={33} />
+				</a>
+			</Link>
+			<div className={headerStyles.menu}>
+				<ul className={headerStyles.navMenu}>
+					<div className={headerStyles.dismiss}>
+						<button>
+							<FiX style={{ fontSize: "1rem" }} />
+						</button>
+					</div>
+					{navs.map(({ name, href }) => (
+						<li className={headerStyles.navItem} key={name}>
+							<Link href={href} passHref>
+								<a className={headerStyles.navLink}>{name}</a>
+							</Link>
+						</li>
+					))}
+				</ul>
+				<button className={headerStyles.requestBtn}>Request a quote</button>
+			</div>
+			<div className={headerStyles.hamburger}>
+				<span className={headerStyles.bar}></span>
+				<span className={headerStyles.bar}></span>
+				<span className={headerStyles.bar}></span>
+			</div>
+		</nav>
+	</header>
 );
 
 export default Header;
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -6,23 +6,23 @@ import workStyles from "../../styles/Work.module.scss";
 import Tags from "../../ components/Tags/Tags";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Seo title="Our iOS, Android &amp; Web Development Work" />
-      <div className={workStyles.container}>
-        <div className={workStyles.text}>
-          <p>Here’s 5% of</p>
-          <p>our published work.</p>
-          <p>See 100% of our power.</p>
-        </div>
+	return (
+		<div>
+			<Seo title="Our iOS, Android &amp; Web Development Work" />
+			<div className={workStyles.container}>
+				<div className={workStyles.text}>
+					<p>Here’s 5% of</p>
+					<p>our published work.</p>
+					<p>See 100% of our power.</p>
+				</div>
 
-        <div>
-          <Tags />
-        </div>
-        <div>work results</div>
-      </div>
-    </div>
-  );
+				<div>
+					<Tags />
+				</div>
+				<div>work results</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
